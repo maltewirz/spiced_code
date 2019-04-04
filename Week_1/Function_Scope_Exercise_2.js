7bfaac98a90144e7b9f89bd5739e9b0f35851207
Malte Wirz <mail@maltewirz.com>
2019-04-04 09:24:30 +0200

    passed callback to timeout function as suggested

--- Week_1/Function_Scope_Exercise_2.js
+++ Week_1/Function_Scope_Exercise_2.js
@@ -2,11 +2,34 @@
 Write a function that takes another function as a parameter. It should wait 1.5 seconds and then run the function that was passed in.
 */
 
-function waitthenrun(callback) {
-    setTimeout(first, 1500);
-    function first() {
-        console.log("i'm first");
-    }
+function waitthenrun(callback_fn) {
+    setTimeout(callback_fn, 1500);
 }
 
-waitthenrun();
+waitthenrun(function first() {
+    console.log("i'm first");
+});
+
+// function waitthenrun(callback) {
+//     setTimeout(first, 1500);
+//     function first() {
+//         console.log("i'm first");
+//     }
+// }
+//
+//
+//
+// // setTimeout(function() {
+// //     console.log('hello');
+// // }, 1000);
+//
+// function add(a, b) {
+//     console.log(a + b);
+//     return a + b;
+// }
+//
+// function waitthenrun(callback) {
+//     setTimeout(add(20, 25), 1500);
+// }
+//
+// waitthenrun();
