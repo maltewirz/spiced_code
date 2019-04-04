4513cdd6aee6f76fac4f2a1338f491d72552e478
Malte Wirz <mail@maltewirz.com>
2019-04-04 13:26:53 +0200

    code clean up

--- Week_1/Exercise_Control_Flow_1.js
+++ Week_1/Exercise_Control_Flow_1.js
@@ -3,35 +3,26 @@ Write a function named logType that expects a single argument and logs a differe
 */
 
 function logType(val) {
-    if (typeof val == "number" && isNaN(val) == false) {                   //Number works
-        console.log("number");
-
-    } else if (val === null) {                      //null works
-        console.log("null");
-
-    } else if (typeof val == "string") {            //string works
-        console.log("string");
-
-    } else if (Array.isArray(val)) {                //array works
-        console.log("array");
-
-    } else if (typeof val == "object") {            //object works
-        console.log("object");
-
-    } else if (typeof val == "function") {          //function works
-        console.log("function");
-
-    } else if (typeof val == "undefined") {         //undefined works
-        console.log("undefined");
-
-    } else if (typeof val == "boolean") {            //boolean works
-        console.log("boolean");
-
-    } else if (Number.isNaN(val)) {                         // NaN works
-        console.log("not a number");
-
+    if (val === null) {
+        console.log("null!");
+    } else if (Array.isArray(val)) {
+        console.log("array!");
+    } else if (typeof val == "object") {
+        console.log("object!");
+    } else if (Number.isNaN(val)) {
+        console.log("not a number!");
+    } else if (typeof val == "number") {
+        console.log("number!");
+    } else if (typeof val == "string") {
+        console.log("string!");
+    } else if (typeof val == "boolean") {
+        console.log("boolean!");
+    } else if (typeof val == "undefined") {
+        console.log("undefined!");
+    } else if (typeof val == "function") {
+        console.log("function!");
     } else {
-        console.log("I have no idea");                     // No idea works
+        console.log("i have no idea!");
     }
 }
 
@@ -39,8 +30,10 @@ logType(7722);
 logType(null);
 logType("string");
 logType(["Saab", "Volvo", "BMW"]);
-logType({type:"Fiat", model:"500", color:"white"});
-logType(function myFunction(a, b) {return a * b;});
+logType({ type: "Fiat", model: "500", color: "white" });
+logType(function myFunction(a, b) {
+    return a * b;
+});
 logType(undefined);
 logType(true);
 logType(NaN);
