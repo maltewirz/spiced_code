71f4562072beed3b02ff654b302cb25163eabe97
Malte Wirz <mail@maltewirz.com>
2019-04-10 12:12:57 +0200

    Ticker solution from David

--- Week_2/ticker/script.js
+++ Week_2/ticker/script.js
@@ -1,42 +1,45 @@
 (function() {
-    var headlines = document.getElementById("headlines"); // select div headlines
-    var links = headlines.getElementsByTagName("A"); // selects links, array-like
-    var left = headlines.offsetLeft; // returns the number of pixels that the upper left corner of the current element is offset to the left
-    var animID; //the animation counter to stop and resume.
+    ticker("ticker1", -1);
 
-    function moveHeadlines() {
-        left--; //it reduces left div, thereby pushing it out
-        if (left <= -links[0].offsetWidth) {
-            left = 0; //if left div far enough out: remove the first link and append it
-            var first = headlines.removeChild(links[0]);
-            headlines.appendChild(first);
-        }
-        headlines.style.left = left + "px";
-        // requestAnimationFrame(moveHeadlines); //it stops when it is not running
-        animID = requestAnimationFrame(moveHeadlines);
-    }
+    ticker("ticker2", 1);
 
-    //this stops the headlines scroll while mouseenter true
-    for (var i = 0; i < links.length; i++) {
-        //loops over the links so that i can recognize which one is selected
-        links[i].addEventListener("mouseenter", function() {
-            cancelAnimationFrame(animID);
-            var link = event.target; //specifies to which target this change shall be applied.
-            link.style.color = "blue";
-            link.style.textDecoration = "underline";
+    function ticker(id, step) {
+        var ticker = document.getElementById(id);
+        var headlines = ticker.querySelector(".headlines");
+        var links = headlines.getElementsByTagName("A");
+        var curX = headlines.offsetLeft;
+        var headlinesWidth = headlines.offsetWidth;
+        var tickerWidth = ticker.offsetWidth;
+        var linkWidth =
+            step < 0
+                ? links[0].offsetWidth
+                : links[links.length - 1].offsetWidth;
+        var animId;
+
+        headlines.addEventListener("mouseenter", function() {
+            cancelAnimationFrame(animId);
         });
-    }
 
-    // this starts the headlines scroll when mouseenter false
-    for (var j = 0; j < links.length; j++) {
-        links[j].addEventListener("mouseleave", function() {
-            headlines.style.textDecoration = "none";
-            var linkTwo = event.target;
-            linkTwo.style.color = "black";
-            linkTwo.style.textDecoration = "none";
+        headlines.addEventListener("mouseleave", function() {
             moveHeadlines();
         });
-    }
 
-    moveHeadlines();
+        moveHeadlines();
+
+        function moveHeadlines() {
+            curX += step;
+            if (step < 0 && curX < -linkWidth) {
+                curX += linkWidth;
+                headlines.appendChild(links[0]);
+                linkWidth = links[0].offsetWidth;
+            }
+            if (step > 0 && curX + headlinesWidth > tickerWidth + linkWidth) {
+                curX -= linkWidth;
+                headlines.insertBefore(links[links.length - 1], links[0]);
+                linkWidth = links[links.length - 1].offsetWidth;
+            }
+            headlines.style.left = curX + "px";
+            animId = requestAnimationFrame(moveHeadlines);
+        }
+    }
 })();
