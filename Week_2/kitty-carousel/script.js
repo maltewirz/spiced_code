e266df5df718a27e28ed7d559a8bd83153498f90
Malte Wirz <mail@maltewirz.com>
2019-04-11 20:40:44 +0200

    first working version

--- Week_2/kitty-carousel/script.js
+++ Week_2/kitty-carousel/script.js
@@ -2,39 +2,28 @@
     var kitties = document.getElementsByClassName("kitty");
     var cur = 0;
 
-    setTimeout(moveKitties, 5000);
+    setTimeout(moveKitties, 1000);
 
     function moveKitties() {
         console.log("The current one is " + cur);
         //removes onscreen class from the element that currently has it and adds exit class to the that same element.
         kitties[cur].classList.remove("onscreen");
         kitties[cur].classList.add("exit");
-        //1. Adds onscreen class to the next kitty element.
-        kitties[cur].classList.add("onscreen");
-
-        // kitties[cur].addEventListener("transitionend", function(e) {
-        //     if (e.target.classList.contains("exit")) {
-        //         e.taget.classList.remove("exit");
-        //         setTimeout(moveKitties, 2000);
-        //     }
-        // });
-
-        // kitties[cur].addEventListener("transitionend", function(e) {
-        //     if (e.target.classList.contains("exit")) {
-        //         e.taget.classList.remove("exit");
-        //         setTimeout(moveKitties, 2000);
-        //     }
-        // });
-        //remove onscreen class from elelment that currently
-        //add exit class to the one just lost
-
+        // when transition to exit is done,remove exit class -> default
+        kitties[cur].addEventListener("transitionend", function(e) {
+            // console.log("transitionend");
+            if (e.target.classList.contains("exit")) {
+                e.target.classList.remove("exit");
+            }
+        });
+        //cur counter goes up, until end of length, then reset to 0
         cur++;
         if (cur >= kitties.length) {
             cur = 0;
         }
-        // add onscreen class to the next one.
+        //Adds onscreen class to the next kitty element.
         console.log("The next one is " + cur);
-
+        kitties[cur].classList.add("onscreen");
         setTimeout(moveKitties, 2000);
     }
 })();
