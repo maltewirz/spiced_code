a9cbdd6491bf14a24d07013d44db30c0958f82e8
Malte Wirz <mail@maltewirz.com>
2019-04-17 16:09:04 +0200

    updated Exercise 2 to use the 'try catch' method

--- Week_3/json/JSON_Exercise_2/script.js
+++ Week_3/json/JSON_Exercise_2/script.js
@@ -1,19 +1,36 @@
 (function() {
     var result;
-    askForNumber();
+
     function askForNumber() {
         var num = prompt('Please enter a number between 1 and 10');
         if (num >= 1 && num <= 10 && num == parseInt(num)) {
-            translateNumberToGerman(num);
             return num;
         }
-        console.log("error, this is not number");
-        askForNumber();
+        throw new Error('Bad number');
     }
 
-    function translateNumberToGerman(engNr) {
-        var translations = ['', 'eins', 'zwei', 'drei', 'vier', 'fünf', 'sechs', 'sieben', 'acht', 'neun', 'zehn'];
-        result = translations[engNr];
-        $(".results").html(result);
+    var translations = {
+        1:'eins',
+        2: 'zwei',
+        3: 'drei',
+        4: 'vier',
+        5: 'fünf',
+        6: 'sechs',
+        7: 'sieben',
+        8: 'acht',
+        9: 'neun',
+        10:'zehn'
+    };
+
+    function translateNumberToGerman() {
+        try { //return number
+            result = translations[askForNumber()];
+            $(".results").html(result);
+        } catch (e) { //what to do in error
+            console.log("error");
+            translateNumberToGerman();
+        }
     }
+    translateNumberToGerman();
+
 })();
