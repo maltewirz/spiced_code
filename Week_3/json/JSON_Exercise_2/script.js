2b7bc0734e5d4b48c47eb8884765cec327920730
Malte Wirz <mail@maltewirz.com>
2019-04-19 14:08:34 +0200

    JSON exercise feedback implementation

--- Week_3/json/JSON_Exercise_2/script.js
+++ Week_3/json/JSON_Exercise_2/script.js
@@ -1,36 +1,36 @@
 (function() {
-    var result;
-
     function askForNumber() {
-        var num = prompt('Please enter a number between 1 and 10');
+        var num = prompt("Please enter a number between 1 and 10");
         if (num >= 1 && num <= 10 && num == parseInt(num)) {
             return num;
         }
-        throw new Error('Bad number');
+        throw new Error("Bad number");
     }
 
     var translations = {
-        1:'eins',
-        2: 'zwei',
-        3: 'drei',
-        4: 'vier',
-        5: 'fünf',
-        6: 'sechs',
-        7: 'sieben',
-        8: 'acht',
-        9: 'neun',
-        10:'zehn'
+        1: "eins",
+        2: "zwei",
+        3: "drei",
+        4: "vier",
+        5: "fünf",
+        6: "sechs",
+        7: "sieben",
+        8: "acht",
+        9: "neun",
+        10: "zehn"
     };
 
     function translateNumberToGerman() {
-        try { //return number
-            result = translations[askForNumber()];
-            $(".results").html(result);
-        } catch (e) { //what to do in error
+        try {
+            //return number
+            var result = translations[askForNumber()];
+            console.log(result);
+            return result;
+        } catch (e) {
+            //what to do in error
             console.log("error");
             translateNumberToGerman();
         }
     }
     translateNumberToGerman();
-
 })();
