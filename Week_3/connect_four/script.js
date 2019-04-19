c143ec7932a398acf1efea80b2035c7a609d13bb
Malte Wirz <mail@maltewirz.com>
2019-04-19 12:22:44 +0200

    added diagonal victory check

--- Week_3/connect_four/script.js
+++ Week_3/connect_four/script.js
@@ -1,62 +1,89 @@
 (function() {
-    $('.column').eq(0).children(); //use this to find colum 0
+    //keep track of current player
+    var currentPlayer = "player1";
+    function switchPlayers() {
+        if (currentPlayer == "player1") {
+            currentPlayer = "player2";
+        } else {
+            currentPlayer = "player1";
+        }
+    }
 
-    $('.column').on('click', function(e){
-        // e.currentTarget this give the column the user has clicked on
-        var slotsInColumn = $(e.currentTarget).find('.slot');
-
+    //column selection: Loop backwards to find slot without p1/2 class
+    $(".column").on("click", function(e) {
+        // currentTarget: clicked column.   .find('.slot') select all column slots
+        var slotsInColumn = $(e.currentTarget).children();
         for (var i = 5; i >= 0; i--) {
-            if (!slotsInColumn.eq(i).hasClass('player1')
-                &&
-                !slotsInColumn.eq(i).hasClass('player2')
-            )   {
+            //loop from bottom to top + check if class assigned.
+            //if slot without p1/2 class -> add class and break loop
+            if (
+                !slotsInColumn.eq(i).hasClass("player1") &&
+                !slotsInColumn.eq(i).hasClass("player2")
+            ) {
                 slotsInColumn.eq(i).addClass(currentPlayer);
                 break;
             }
         }
-        if (i == -1) {
-            return;
-        }
 
+        //check if already won
         if (checkforVictory(slotsInColumn)) {
-            //do victory dance
+            console.log("victory columns");
             return;
         } else {
-            var slotsInRow = $('.row' + i).find('slot');
+            var slotsInRow = $(".row" + i);
             if (checkforVictory(slotsInRow)) {
-                //do victory dacne
+                console.log("victory rows");
                 return;
             } else {
-                //diagonal check
+                var slots = $(".slot");
+                victoryDiagonal();
+                function victoryDiagonal() {
+                    for (var i = 0; i <= 20; i++) {
+                        if (i == 3 || i == 9 || i == 15) {
+                            i += 3;
+                        }
+                        if (slots.eq(i).hasClass("player1")) {
+                            if (slots.eq(i + 7).hasClass("player1")) {
+                                if (slots.eq(i + 14).hasClass("player1")) {
+                                    if (slots.eq(i + 21).hasClass("player1")) {
+                                        console.log("victory DiagonalDown");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    for (var j = 3; j <= 23; j++) {
+                        if (j == 6 || j == 12 || j == 18) {
+                            j += 3;
+                        }
+                        if (slots.eq(j).hasClass("player1")) {
+                            if (slots.eq(j + 5).hasClass("player1")) {
+                                if (slots.eq(j + 10).hasClass("player1")) {
+                                    if (slots.eq(j + 15).hasClass("player1")) {
+                                        console.log("victory DiagonalUp");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-
         switchPlayers();
     });
 
-    //keep track of current player is
-    var currentPlayer = 'player1';
-
-    function switchPlayers() {
-        if (currentPlayer == 'player1') {
-            currentPlayer = 'player2';
-        } else {
-            currentPlayer = 'player1';
+    // function for victorycheck
+    function checkforVictory(slots) {
+        var count = 0;
+        for (var i = 0; i < slots.length; i++) {
+            if (slots.eq(i).hasClass(currentPlayer)) {
+                count++;
+                if (count == 4) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
         }
     }
 })();
-
-// another way to check
-function checkforVictory(slots) {
-    var count = 0;
-    for (var i = 0; i > slots.length; i++) {
-        if (slots.eq(i).hasClass(currentPlayer)) {
-            count++;
-            if (count == 4) {
-                return true;
-            }
-        } else {
-            count = 0;
-        }
-    }
-}
