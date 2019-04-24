18a612df3f4bd63905084e116b8e0a7342d60ccb
Malte Wirz <mail@maltewirz.com>
2019-04-24 17:10:35 +0200

    spotify - first working version

--- Week_4/spotify/script.js
+++ Week_4/spotify/script.js
@@ -1,34 +1,58 @@
 (function() {
     var nextUrl;
+    var resultsHtml = "";
+    var coverPic;
 
     var button = $(".submit-button");
     button.on("click", function() {
         var userInput = $("input[name='user-input']").val(); //target the name
         var dropdown = $(".artist-or-album").val();
-        //we have to send proxy 3 pieces of info
-        // 1. url -- just defines what website we're mkaing a request to.
-        // 2. user input -- the text that the user put in the input field.
-        // 3. dropdown -- the user's choice from the dropwdown menu.
+
         $.ajax({
             url: "https://elegant-croissant.glitch.me/spotify",
             data: {
-                //represents input did we get from the user#
+                //represents input we get from the user#
                 query: userInput,
                 type: dropdown
             },
             //success is a function that runs once we heard a API response
             success: function(payload) {
-                payload = payload.artists || payload.albums;
                 //if lefthand side is defined -> && then do.
-                nextUrl =
-                    payload.next &&
-                    payload.next.replace(
-                        "https://api.spotify.com/v1/search",
-                        "https://elegant-croissant.glitch.me/spotify"
-                    );
-                console.log(nextUrl);
+                payload = payload.artists || payload.albums;
+
+                for (var i = 0; i < payload.items.length; i++) {
+                    if (typeof payload.items[i].images[1] == "undefined") {
+                        coverPic = "/logo.png";
+                    } else {
+                        coverPic = payload.items[i].images[1].url;
+                    }
+
+                    resultsHtml +=
+                        "<div class='result'>" +
+                        "<img src=" +
+                        coverPic +
+                        ">" +
+                        '<a href="' +
+                        payload.items[i].external_urls.spotify +
+                        '">' +
+                        '<div class="text">' +
+                        payload.items[i].name +
+                        "</div></a></div>";
+                    $(".results").html(resultsHtml);
+                }
+
+                ///////-----------------------------
+                //more stuff for later
+                // nextUrl =
+                //     payload.next &&
+                //     payload.next.replace(
+                //         "https://api.spotify.com/v1/search",
+                //         "https://elegant-croissant.glitch.me/spotify"
+                //     );
+                // console.log(nextUrl);
                 // nextUrl to make the second ajax request thorugh "more"button
                 //wrap ajax request in function for reusing. in the second ajax, not data is needed.
+                ///////-----------------------------
             }
         });
     });
