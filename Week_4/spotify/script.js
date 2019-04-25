bbc81c9f9b92dafd767eb5a4fb76547b4990c33b
Malte Wirz <mail@maltewirz.com>
2019-04-25 11:35:25 +0200

    spotify - code cleanup

--- Week_4/spotify/script.js
+++ Week_4/spotify/script.js
@@ -17,42 +17,10 @@
                 url: urlInputTwo,
                 success: function(payload) {
                     payload = payload.artists || payload.albums;
-                    for (var i = 0; i < payload.items.length; i++) {
-                        if (typeof payload.items[i].images[1] == "undefined") {
-                            coverPic = "/logo.png";
-                        } else {
-                            coverPic = payload.items[i].images[1].url;
-                        }
-
-                        resultsHtml +=
-                            "<div class='result'>" +
-                            "<img src=" +
-                            coverPic +
-                            ">" +
-                            '<a href="' +
-                            payload.items[i].external_urls.spotify +
-                            '">' +
-                            '<div class="text">' +
-                            payload.items[i].name +
-                            "</div></a></div>";
-                    }
+                    getHtml(payload);
                     $(".results").append(resultsHtml);
-
-                    nextUrl =
-                        payload.next &&
-                        payload.next.replace(
-                            "https://api.spotify.com/v1/search",
-                            "https://elegant-croissant.glitch.me/spotify"
-                        );
-                    if (nextUrl != null) {
-                        $(".results")
-                            .last()
-                            .append("<div class='more'>More</div>");
-                        $(".more").on("click", function() {
-                            $(".more").remove();
-                            fetchAjaxTwo(nextUrl);
-                        });
-                    }
+                    setNextUrl(payload);
+                    nextUrlFunction(nextUrl);
                 }
             });
         }
@@ -66,61 +34,67 @@
             $.ajax({
                 url: urlInput,
                 data: {
-                    //represents input we get from the user#
                     query: userInput,
                     type: dropdown
                 },
-                //success is a function that runs once we heard a API response
                 success: function(payload) {
-                    //if lefthand side is defined -> && then do.
                     payload = payload.artists || payload.albums;
-                    //Search head
                     $(".results").append(
                         "<div class='search-notice'>Results for " +
                             userInput +
                             "</div>"
                     );
-                    for (var i = 0; i < payload.items.length; i++) {
-                        if (typeof payload.items[i].images[1] == "undefined") {
-                            coverPic = "/logo.png";
-                        } else {
-                            coverPic = payload.items[i].images[1].url;
-                        }
-
-                        resultsHtml +=
-                            "<div class='result'>" +
-                            "<img src=" +
-                            coverPic +
-                            ">" +
-                            '<a href="' +
-                            payload.items[i].external_urls.spotify +
-                            '">' +
-                            '<div class="text">' +
-                            payload.items[i].name +
-                            "</div></a></div>";
-                    }
+                    getHtml(payload);
                     $(".results").append(resultsHtml);
-
-                    nextUrl =
-                        payload.next &&
-                        payload.next.replace(
-                            "https://api.spotify.com/v1/search",
-                            "https://elegant-croissant.glitch.me/spotify"
-                        );
-                    if (nextUrl != null) {
-                        $(".results")
-                            .last()
-                            .append("<div class='more'>More</div>");
-                        $(".more").on("click", function() {
-                            $(".more").remove();
-                            fetchAjaxTwo(nextUrl);
-                        });
-                    }
+                    setNextUrl(payload);
+                    nextUrlFunction(nextUrl);
                 },
                 error: function() {
                     $(".results").html("<div>No results</div>");
                 }
             });
         }
+        function nextUrlFunction(data) {
+            if (data != null) {
+                $(".results")
+                    .last()
+                    .append("<div class='more'>More</div>");
+                $(".more").on("click", function() {
+                    $(".more").remove();
+                    fetchAjaxTwo(nextUrl);
+                });
+            }
+        }
     });
+
+    function getHtml(data) {
+        for (var i = 0; i < data.items.length; i++) {
+            if (typeof data.items[i].images[1] == "undefined") {
+                coverPic = "/logo.png";
+            } else {
+                coverPic = data.items[i].images[1].url;
+            }
+
+            resultsHtml +=
+                "<div class='result'>" +
+                "<img src=" +
+                coverPic +
+                ">" +
+                '<a href="' +
+                data.items[i].external_urls.spotify +
+                '">' +
+                '<div class="text">' +
+                data.items[i].name +
+                "</div></a></div>";
+        }
+    }
+
+    function setNextUrl(data) {
+        nextUrl =
+            data.next &&
+            data.next.replace(
+                "https://api.spotify.com/v1/search",
+                "https://elegant-croissant.glitch.me/spotify"
+            );
+    }
 })();
