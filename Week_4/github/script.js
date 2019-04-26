0e84ca3511bc23c1d758a1628ca44755b3700837
Malte Wirz <mail@maltewirz.com>
2019-04-26 13:11:11 +0200

    github part I working

--- Week_4/github/script.js
+++ Week_4/github/script.js
@@ -26,10 +26,9 @@
                 authorization: "Basic " + btoa(username + ":" + password)
             },
             success: function(payload) {
-                console.log(payload);
-                // full_name is important
-                // avatar_url is important. this ithe profile pricture of the user that made the respository
-                // use handlebars to render the full name and the avatar url.
+                $(".container").append(
+                    Handlebars.templates.queryA({ payload: payload })
+                );
             }
         });
     });
