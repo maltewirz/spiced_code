a4f01cc279282e0749f8bd91d1fec75627073467
Malte Wirz <mail@maltewirz.com>
2019-04-26 11:53:40 +0200

    github template

---
+++ Week_4/github/script.js
@@ -0,0 +1,36 @@
+(function() {
+    Handlebars.templates = Handlebars.templates || {};
+
+    var templates = document.querySelectorAll(
+        'script[type="text/x-handlebars-template"]'
+    );
+
+    Array.prototype.slice.call(templates).forEach(function(script) {
+        Handlebars.templates[script.id] = Handlebars.compile(script.innerHTML);
+    });
+    ////////////////////////////////////////////////////////////////////////////
+    // Do not touch
+    ////////////////////////////////////////////////////////////////////////////
+
+    $(".submit-button").on("click", function() {
+        var username = $('input[name="username"]').val();
+        var password = $('input[name="password"]').val();
+        var userToSearch = $('input[name="user-to-search"]').val();
+
+        var rootUrl = "https://api.github.com";
+        var endpoint = "/users/" + userToSearch + "/repos";
+
+        $.ajax({
+            url: rootUrl + endpoint,
+            headers: {
+                authorization: "Basic " + btoa(username + ":" + password)
+            },
+            success: function(payload) {
+                console.log(payload);
+                // full_name is important
+                // avatar_url is important. this ithe profile pricture of the user that made the respository
+                // use handlebars to render the full name and the avatar url.
+            }
+        });
+    });
+})();
