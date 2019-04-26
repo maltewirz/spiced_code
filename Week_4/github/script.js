a06adca085bfee4fcd448a2d502be37034d3f75c
Malte Wirz <mail@maltewirz.com>
2019-04-26 13:31:42 +0200

    github 1.5

--- Week_4/github/script.js
+++ Week_4/github/script.js
@@ -19,6 +19,7 @@
 
         var rootUrl = "https://api.github.com";
         var endpoint = "/users/" + userToSearch + "/repos";
+        // var commits = "/users/" + userToSearch + "/" + ;
 
         $.ajax({
             url: rootUrl + endpoint,
@@ -26,9 +27,19 @@
                 authorization: "Basic " + btoa(username + ":" + password)
             },
             success: function(payload) {
+                console.log(payload);
                 $(".container").append(
                     Handlebars.templates.queryA({ payload: payload })
                 );
+                $(".results").on("click", function(e) {
+                    console.log("click");
+                    // var ex = e.currentTarget;
+                    console.log(e.currentTarget);
+                    // $('.results').eq(0).text()
+                    // $.ajax({
+                    //     url:
+                    // });
+                });
             }
         });
     });
