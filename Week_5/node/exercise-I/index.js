97a86c78b820cb9bc6e1f83d0dfe5bee116a27b8
Malte Wirz <mail@maltewirz.com>
2019-04-30 09:23:57 +0200

    countdown - added process.arg to receive cmd objects and added 'for e in obj' loops to DRY code

--- Week_5/node/exercise-I/index.js
+++ Week_5/node/exercise-I/index.js
@@ -1,3 +1,27 @@
-const myMod = require("./myModule.js");
+const url = require("url");
+const querystring = require("querystring");
 
-myMod.transform("http://127.0.0.1:8080/test?a=100&b=200");
+//parsing input from cmd and exiting if no input
+let str = process.argv[2];
+if (!str) {
+    process.exit();
+}
+let parsed = url.parse(str);
+
+//looping through parsed url object while ignoring non relevant object keys
+for (let e in parsed) {
+    if (
+        parsed.hasOwnProperty(e) &&
+        parsed[e] != null &&
+        e != "slashes" &&
+        e != "href" &&
+        e != "search"
+    ) {
+        console.log(`The ${e} is ${parsed[e]}`);
+    }
+}
+//another loop for object in object
+let query = querystring.parse(parsed.query);
+for (let e in query) {
+    console.log(`The value of the ${e} parameter is ${query[e]}`);
+}
