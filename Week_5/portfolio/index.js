84844a9d43e83c000f2a077f48541064a77b30d4
Malte Wirz <mail@maltewirz.com>
2019-05-03 14:57:04 +0200

    Portfolio Part I

--- Week_5/portfolio/index.js
+++ Week_5/portfolio/index.js
@@ -16,36 +16,62 @@ http.createServer((req, res) => {
         res.end("Intruder Alert");
         return;
     }
+
     //actual file handling
     fs.stat(myPath, (err, stats) => {
+        // handling wrong file/url name
+        if (typeof stats == "undefined") {
+            res.statusCode = 404;
+            res.end();
+            return;
+        }
+        let query = __dirname + "/projects" + req.url;
         if (err) {
             console.log(err);
-            res.statusCode = 404;
+            res.statusCode = 302;
+            res.end();
         }
-        console.log("i'm live");
 
-        const readStream = fs.createReadStream(
-            __dirname + "/projects/spotify_handlebars/logo.png"
-        );
-
-        //send request.
-        //determine if file or directory.
-        //// if file: pipe it out, set header and content type correctly
-        //from extension of path.parse, tels you which header to set.
-        ///if directory: standard webserver behaviour:open index.html
-        ////// if url == directory, serve index.html. if no index.html, server 404.
-        //use stat to check for index.html. otherwise use readstreams error handlnig to serve 404
-    });
-
-    //example code
-    res.setHeader("content-type", "image/jpeg");
-    const readStream = fs.createReadStream(
-        __dirname + "/projects/spotify_handlebars/logo.png"
-    );
-    readStream.pipe(res);
-    readStream.on("error", err => {
-        console.log(err);
-        res.statusCode = 404;
-        res.end();
+        if (stats.isFile()) {
+            console.log("requesting a file");
+            //checks the request file extension
+            const ext = path.parse(__dirname + "/projects" + req.url).ext;
+            //checks ext and replaces Header appropriately
+            if (ext == ".html") {
+                res.setHeader("content-type", "text/html");
+            } else if (ext == ".css") {
+                res.setHeader("content-type", "text/css");
+            } else if (ext == ".js") {
+                res.setHeader("content-type", "text/javascript");
+            } else if (ext == ".png") {
+                res.setHeader("content-type", "image/png");
+            }
+        } else if (stats.isDirectory()) {
+            //add a index.html if a dir request with /
+            if (req.url[req.url.length - 1] == "/") {
+                query += "index.html";
+            }
+            //if a dir but / missing
+            console.log("requesting a dir");
+            if (req.url[req.url.length - 1] != "/") {
+                console.log("causing the crash");
+                res.setHeader("Location", req.url + "/");
+                res.statusCode = 302;
+                res.end();
+            }
+        }
+        // else {
+        //     res.statusCode = 404;
+        //     res.end();
+        // }
+        //serving the file
+        const readStream = fs.createReadStream(query);
+        res.statusCode = 200;
+        readStream.pipe(res);
+        readStream.on("error", err => {
+            console.log(err);
+            res.statusCode = 404;
+            res.end();
+        });
     });
 }).listen(8080, () => console.log(`I'm listening`));
