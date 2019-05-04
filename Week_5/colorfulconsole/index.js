367de89b68078e83c2fda4c6200af664b978f1de
Malte Wirz <mail@maltewirz.com>
2019-05-04 12:24:43 +0200

    little css changes

--- Week_5/colorfulconsole/index.js
+++ Week_5/colorfulconsole/index.js
@@ -13,23 +13,25 @@ const server = http
             console.log("GET request...");
             res.setHeader("content-type", "text/html");
             res.statusCode = 200;
-            res.write(`<!doctype html>
-<html>
-<title>Colors</title>
-<form method="POST">
-  <input type="text" name="text">
-  <select name="color">
-    <option value="red">red</option>
-    <option value="blue">blue</option>
-    <option value="green">green</option>
-    <option value="yellow">yellow</option>
-    <option value="gray">gray</option>
-    <option value="magenta">magenta</option>
-    <option value="cyan">cyan</option>
-  </select>
-  <button type="submit">Go</button>
-</form>
-</html>`);
+            res.write(`
+                <!doctype html>
+                <html>
+                <title>Colors</title>
+                <form method="POST">
+                  <input type="text" name="text">
+                  <select name="color">
+                    <option value="red">red</option>
+                    <option value="blue">blue</option>
+                    <option value="green">green</option>
+                    <option value="yellow">yellow</option>
+                    <option value="gray">gray</option>
+                    <option value="magenta">magenta</option>
+                    <option value="cyan">cyan</option>
+                  </select>
+                  <button type="submit">Go</button>
+                </form>
+                </html>
+            `);
             res.end();
         }
         if (req.method == "POST") {
