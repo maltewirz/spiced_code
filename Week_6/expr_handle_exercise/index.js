d85d4bbc711ae59c13cf3e535a8fb9f968aa042d
Malte Wirz <mail@maltewirz.com>
2019-05-07 17:21:18 +0200

    Express Handlebars - added 404 for unfound pages

--- Week_6/expr_handle_exercise/index.js
+++ Week_6/expr_handle_exercise/index.js
@@ -13,6 +13,7 @@ let fileNames = [];
 for (let i = 0; i < files.length; i++) {
     fileNames.push(files[i].name);
 }
+
 //reading the description jsons and writing page
 app.get("/projects/:name/description", (req, res) => {
     let descrData = require(`${projectsPath}/${
@@ -26,11 +27,17 @@ app.get("/projects/:name/description", (req, res) => {
     });
 });
 
+//handling all known pages
 app.get("/", (req, res) => {
     res.render("home", {
         layout: "main",
         name: fileNames
     });
+});
+
+//handling of unfound pages
+app.use(function(req, res) {
+    res.status(404).send("That's a 404");
 });
 
 app.listen(8080, () => {
