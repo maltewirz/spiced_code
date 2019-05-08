8ee9e0496ee02931a66bc7c47ed79fb52693e793
Malte Wirz <mail@maltewirz.com>
2019-05-08 17:48:55 +0200

    Twitter alternative solution

--- week-6/twapi/index.js
+++ week-6/twapi/index.js
@@ -13,20 +13,39 @@ app.get("/data.json", (req, res) => {
                 if (err) {
                     res.sendStatus(500);
                 } else {
-                    tweets = tweets
-                        .filter(item => {
-                            return item.entities.urls.length == 1;
-                        })
-                        .map(item => {
-                            return {
-                                text: item.full_text.replace(
-                                    /(?:https?|ftp):\/\/[\n\S]+/g,
-                                    ""
-                                ),
-                                href: item.entities.urls[0].url
-                            };
-                        });
-                    res.json(tweets);
+                    let data = [];
+                    for (let i = 0; i < tweets.length; i++) {
+                        if (
+                            tweets[i].entities.urls != undefined &&
+                            tweets[i].entities.urls.length != 0
+                        ) {
+                            let cleanTweet;
+                            let fullTweet = tweets[i].full_text;
+                            let urlTweet = tweets[i].entities.urls[0].url;
+
+                            if (
+                                tweets[i].entities.media != undefined &&
+                                tweets[i].entities.media != 0
+                            ) {
+                                let medUrlTweet =
+                                    tweets[i].entities.media[0].url;
+                                cleanTweet = fullTweet
+                                    .replace(urlTweet, "")
+                                    .replace(medUrlTweet, "");
+                                let obj = {};
+                                obj.text = cleanTweet;
+                                obj.href = urlTweet;
+                                data.push(obj);
+                            } else {
+                                cleanTweet = fullTweet.replace(urlTweet, "");
+                                let obj = {};
+                                obj.href = urlTweet;
+                                obj.text = cleanTweet;
+                                data.push(obj);
+                            }
+                        }
+                    }
+                    res.json(data);
                 }
             });
         }
