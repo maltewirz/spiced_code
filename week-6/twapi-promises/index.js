27eb35434b92e57233c7384b94f0a620a26fe14c
Malte Wirz <mail@maltewirz.com>
2019-05-09 18:13:00 +0200

    twapi promises - data not yet sorted

--- week-6/twapi-promises/index.js
+++ week-6/twapi-promises/index.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const app = express();
 const twApi = require("./twApi");
-const util = require("util");
-const getTwitToken = util.promisify(twApi.getToken);
-const getTwitTweets = util.promisify(twApi.getTweets);
+// const util = require("util");
+// const getTwitToken = util.promisify(twApi.getToken);
+// const getTwitTweets = util.promisify(twApi.getTweets);
+const getTwitToken = twApi.getToken;
+const getTwitTweets = twApi.getTweets;
 
 app.use(express.static("./public"));
 
 app.get("/data.json", (req, res) => {
     getTwitToken()
         .then(token => {
-            return getTwitTweets(token);
+            return Promise.all([
+                getTwitTweets(token, "theonion"),
+                getTwitTweets(token, "nytimes"),
+                getTwitTweets(token, "bbcworld")
+            ]);
         })
         .then(tweets => {
+            let theonion = tweets[0];
+            let nytimes = tweets[1];
+            let bbcworld = tweets[2];
+            let mergedArrayOfTweets = theonion.concat(nytimes, bbcworld);
+            tweets = mergedArrayOfTweets;
             tweets = tweets
                 .filter(item => {
                     return item.entities.urls.length == 1;
@@ -42,4 +53,4 @@ app.use(function(req, res) {
     res.status(404).send("That's a 404");
 });
 
-app.listen(8080, () => console.log(`I'm listening`));
+app.listen(8080, () => console.log(`I'm listening twapi promises`));
--- week-6/twapi-promises/twApi.js
+++ week-6/twapi-promises/twApi.js
@@ -1,78 +1,81 @@
 const https = require("https");
 const { consumerKey, consumerSecret } = require("./secrets");
 
-exports.getTweets = function(token, callback) {
-    const req = https.request(
-        {
-            host: "api.twitter.com",
-            path:
-                "/1.1/statuses/user_timeline.json?screen_name=theonion&tweet_mode=extended&limit=1",
-            method: "GET",
-            headers: {
-                "content-type":
-                    "application/x-www-form-urlencoded;charset=UTF-8",
-                authorization: `Bearer ${token}`
+exports.getTweets = function(token, screenName) {
+    return new Promise(function(resolve, reject) {
+        const req = https.request(
+            {
+                host: "api.twitter.com",
+                path: `/1.1/statuses/user_timeline.json?count=5&screen_name=${screenName}&tweet_mode=extended&limit=1`,
+                method: "GET",
+                headers: {
+                    "content-type":
+                        "application/x-www-form-urlencoded;charset=UTF-8",
+                    authorization: `Bearer ${token}`
+                }
+            },
+            res => {
+                if (res.statusCode != 200) {
+                    reject(new Error(res.statusCode));
+                } else {
+                    let body = "";
+                    res.on("data", chunk => (body += chunk)).on("end", () => {
+                        try {
+                            body = JSON.parse(body);
+                            resolve(body);
+                        } catch (err) {
+                            console.log(err);
+                            reject(err);
+                        }
+                    });
+                }
             }
-        },
-        res => {
-            if (res.statusCode != 200) {
-                callback(new Error(res.statusCode));
-            } else {
-                let body = "";
-                res.on("data", chunk => (body += chunk)).on("end", () => {
-                    try {
-                        body = JSON.parse(body);
-                        callback(null, body);
-                    } catch (err) {
-                        console.log(err);
-                        callback(err);
-                    }
-                });
-            }
-        }
-    );
-    req.on("error", err => {
-        console.log(err);
-        callback(err);
+        );
+        req.on("error", err => {
+            console.log(err);
+            reject(err);
+        });
+        req.end();
     });
-    req.end();
 };
 
-exports.getToken = function(callback) {
-    const encoded = Buffer.from(`${consumerKey}:${consumerSecret}`).toString(
-        "base64"
-    );
-    const req = https.request(
-        {
-            host: "api.twitter.com",
-            path: "/oauth2/token",
-            method: "POST",
-            headers: {
-                "content-type":
-                    "application/x-www-form-urlencoded;charset=UTF-8",
-                authorization: `Basic ${encoded}`
+exports.getToken = function() {
+    return new Promise(function(resolve, reject) {
+        const encoded = Buffer.from(
+            `${consumerKey}:${consumerSecret}`
+        ).toString("base64");
+        const req = https.request(
+            {
+                host: "api.twitter.com",
+                path: "/oauth2/token",
+                method: "POST",
+                headers: {
+                    "content-type":
+                        "application/x-www-form-urlencoded;charset=UTF-8",
+                    authorization: `Basic ${encoded}`
+                }
+            },
+            res => {
+                if (res.statusCode != 200) {
+                    reject(new Error(res.statusCode));
+                } else {
+                    let body = "";
+                    res.on("data", chunk => (body += chunk)).on("end", () => {
+                        try {
+                            body = JSON.parse(body);
+                            resolve(body.access_token);
+                        } catch (err) {
+                            reject(err);
+                        }
+                    });
+                }
             }
-        },
-        res => {
-            if (res.statusCode != 200) {
-                callback(new Error(res.statusCode));
-            } else {
-                let body = "";
-                res.on("data", chunk => (body += chunk)).on("end", () => {
-                    try {
-                        body = JSON.parse(body);
-                        callback(null, body.access_token);
-                    } catch (err) {
-                        callback(err);
-                    }
-                });
-            }
-        }
-    );
-    req.on("error", err => {
-        console.log(err);
-        callback(err);
+        );
+        req.on("error", err => {
+            console.log(err);
+            reject(err);
+        });
+        req.write(`grant_type=client_credentials`);
+        req.end();
     });
-    req.write(`grant_type=client_credentials`);
-    req.end();
 };
